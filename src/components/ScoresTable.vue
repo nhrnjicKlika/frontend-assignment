5fd9b3ec9992ee0b63d22c4e05f1a5974a2e54c6
nhrnjicKlika <nihad.hrnjic@klika.ba>
2018-03-30 11:45:37 +0200

    add dropdown for selecting a round

--- src/components/ScoresTable.vue
+++ src/components/ScoresTable.vue
@@ -1,6 +1,9 @@
 <template>
     <div>
-        <p> Scores Table </p>
+        <select v-model = 'selectedRound'>
+            <option value="0"> Select a round </option>
+            <option v-for = 'round in roundsRange' :value="round"> {{ round }} </option>
+        </select>
     </div>
 </template>
 
@@ -10,12 +13,32 @@ import { mapState } from 'vuex'
 
 export default{
 
-    created(){},
+    data(){
+        return{
+            selectedRound: 0,
+            roundsRange: []
+        }
+    },
+
+    created(){
+        for(let i = 0; i < this.scoresTable.length; i++){
+            this.roundsRange = [...this.roundsRange, (i + 1)]
+        }
+    },
 
     computed: {
         ...mapState({
             scoresTable(state){
                 return state.scoresTable
+            },
+
+            selectedRoundData(){
+                let lastRoundIndex = this.scoresTable.length - 1
+                if(this.selectedRound === 0){
+                    return this.scoresTable[lastRoundIndex]
+                }else{
+                    return this.scoresTable[this.selectedRound]
+                }
             }
         })
     }
