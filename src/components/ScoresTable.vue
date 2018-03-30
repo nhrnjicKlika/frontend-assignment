aa268ae2747ad985e58fe75f73360ccb9149a75b
nhrnjicKlika <nihad.hrnjic@klika.ba>
2018-03-30 12:16:11 +0200

    added basic representation of selected round results

--- src/components/ScoresTable.vue
+++ src/components/ScoresTable.vue
@@ -1,9 +1,23 @@
 <template>
     <div>
         <select v-model = 'selectedRound'>
-            <option value="0"> Select a round </option>
-            <option v-for = 'round in roundsRange' :value="round"> {{ round }} </option>
+            <option value="-1"> Select a round </option>
+            <option v-for = 'round in roundsRange' :value="(round)"> {{ (round + 1) }} </option>
         </select>
+
+        <div class = 'matches-listing'>
+            <div v-for = 'match in selectedRoundData' class = 'match'>
+                <div>
+                    <span> {{ getTeamData(match, 0).teamName }} </span>
+                    <span> {{ getTeamData(match, 0).goals }} </span>
+                </div>
+
+                <div>
+                    <span> {{ getTeamData(match, 1).teamName }} </span>
+                    <span> {{ getTeamData(match, 1).goals }} </span>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
@@ -15,14 +29,14 @@ export default{
 
     data(){
         return{
-            selectedRound: 0,
+            selectedRound: -1,
             roundsRange: []
         }
     },
 
     created(){
         for(let i = 0; i < this.scoresTable.length; i++){
-            this.roundsRange = [...this.roundsRange, (i + 1)]
+            this.roundsRange = [...this.roundsRange, i]
         }
     },
 
@@ -34,13 +48,25 @@ export default{
 
             selectedRoundData(){
                 let lastRoundIndex = this.scoresTable.length - 1
-                if(this.selectedRound === 0){
-                    return this.scoresTable[lastRoundIndex]
+                if(this.selectedRound === -1){
+                    return this.scoresTable[lastRoundIndex].matches
                 }else{
-                    return this.scoresTable[this.selectedRound]
+                    return this.scoresTable[this.selectedRound].matches
                 }
             }
         })
+    },
+
+    methods: {
+        getTeamData(match, index){
+            let teamName = Object.keys(match)[index]
+            let goals = match[teamName]
+
+            return{
+                teamName,
+                goals
+            }
+        }
     }
 }
 
